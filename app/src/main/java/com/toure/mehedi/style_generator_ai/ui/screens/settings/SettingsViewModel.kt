ee5decf2d5c2cf8681c58940b51fac1a8cd68e82
mehediT <mehedi.toure@gmail.com>
2026-02-16 00:20:00 +0100

    feat: implement settings screen and view model

---
+++ app/src/main/java/com/toure/mehedi/style_generator_ai/ui/screens/settings/SettingsViewModel.kt
@@ -0,0 +1,32 @@
+package com.toure.mehedi.style_generator_ai.ui.screens.settings
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import javax.inject.Inject
+
+data class SettingsUiState(
+    val isDarkMode: Boolean = false,
+    val notificationsEnabled: Boolean = true
+)
+
+@HiltViewModel
+class SettingsViewModel @Inject constructor() : ViewModel() {
+
+    private val _uiState = MutableStateFlow(SettingsUiState())
+    val uiState: StateFlow<SettingsUiState> = _uiState.asStateFlow()
+
+    fun toggleDarkMode() {
+        _uiState.value = _uiState.value.copy(
+            isDarkMode = !_uiState.value.isDarkMode
+        )
+    }
+
+    fun toggleNotifications() {
+        _uiState.value = _uiState.value.copy(
+            notificationsEnabled = !_uiState.value.notificationsEnabled
+        )
+    }
+}
