3725a4e93fef942727042b24db370d0f4920fbac
mehediT <mehedi.toure@gmail.com>
2026-02-15 23:40:06 +0100

    feat: integrate Hilt for dependency injection

--- app/src/main/java/com/toure/mehedi/style_generator_ai/MainActivity.kt
+++ app/src/main/java/com/toure/mehedi/style_generator_ai/MainActivity.kt
@@ -6,7 +6,9 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import com.toure.mehedi.style_generator_ai.ui.showcase.DesignSystemShowcase
 import com.toure.mehedi.style_generator_ai.ui.theme.AndroidstylegeneratoraiTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
