be76192500283e8a5635f193685f5b9314377f72
mehediT <mehedi.toure@gmail.com>
2026-02-23 11:29:33 +0100

    feat: implement image upload for virtual try-on Using https://tmpfiles.org/api

--- data/src/main/kotlin/com/toure/mehedi/style_generator_ai/data/remote/service/TryOnServiceApi.kt
+++ data/src/main/kotlin/com/toure/mehedi/style_generator_ai/data/remote/service/TryOnServiceApi.kt
@@ -1,11 +1,53 @@
 package com.toure.mehedi.style_generator_ai.data.remote.service
 
 import android.util.Log
+import io.ktor.client.HttpClient
+import io.ktor.client.engine.android.Android
+import io.ktor.client.request.forms.MultiPartFormDataContent
+import io.ktor.client.request.forms.formData
+import io.ktor.client.request.post
+import io.ktor.client.request.setBody
+import io.ktor.client.statement.bodyAsText
+import io.ktor.http.Headers
+import io.ktor.http.HttpHeaders
+import org.json.JSONObject
 import javax.inject.Inject
 
 class TryOnServiceApi @Inject constructor() {
+
+    private val httpClient = HttpClient(Android)
+
     suspend fun tryOn(imageBytes: ByteArray, productId: String): String {
-        Log.d("TryOnServiceApi", "Trying on product $productId with image $imageBytes")
-        TODO("Implement API call")
+        Log.d(TAG, "Uploading image for product $productId (${imageBytes.size} bytes)")
+
+        val responseText = httpClient.post(UPLOAD_URL) {
+            setBody(
+                MultiPartFormDataContent(
+                    formData {
+                        append(
+                            key = "file",
+                            value = imageBytes,
+                            headers = Headers.build {
+                                append(HttpHeaders.ContentType, "image/jpeg")
+                                append(HttpHeaders.ContentDisposition, "filename=\"image.jpg\"")
+                            }
+                        )
+                    }
+                )
+            )
+        }.bodyAsText()
+
+        val url = JSONObject(responseText)
+            .getJSONObject("data")
+            .getString("url")
+            .replace("tmpfiles.org/", "tmpfiles.org/dl/")
+
+        Log.d(TAG, "Uploaded image URL: $url")
+        return url
+    }
+
+    companion object {
+        private const val TAG = "TryOnServiceApi"
+        private const val UPLOAD_URL = "https://tmpfiles.org/api/v1/upload"
     }
 }
