183cf3a43ad710acd0a270a940b2a706adce9fe1
mehediT <mehedi.toure@gmail.com>
2026-02-24 23:58:34 +0100

    docs: expand ToDo.kt with new tasks and ideas
    - Add tasks for making the `fashion_product` bucket public and managing modified pictures
    - Include ideas for embedding prompts and implementing a two-step image generation process
    - Add feature suggestions for downloading or sharing generated images
    - Move existing idea for an article screen under the `TODO` section

--- app/src/main/java/com/toure/mehedi/style_generator_ai/ToDo.kt
+++ app/src/main/java/com/toure/mehedi/style_generator_ai/ToDo.kt
@@ -8,5 +8,10 @@ package com.toure.mehedi.style_generator_ai
 // TODO : Need to handle all error from domain part using try-catch and Exception with sealed Class to log or show ui
 // TODO : Need to handle ui error - When bytes: ByteArray is null in FashionProductDetailViewModel
 
-// IDEA : Article screen from website or link or the app can
-// take a link from a market and get the article
+// TODO : Change bucket fashion_product to public
+// TODO : Bibliotheque of picture modificated
+// TODO : Embed all prompt or else,
+// TODO : Two step before generate image
+// TODO : Download or share the picture generated
+
+// TODO : Article screen from website or link or the app can take a link from a market and get the article
