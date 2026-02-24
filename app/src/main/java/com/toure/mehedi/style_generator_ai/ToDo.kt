ca93b10eb86612a641f4ac5a279a07b7b0b2c36b
mehediT <mehedi.toure@gmail.com>
2026-02-24 02:05:33 +0100

    docs: add ToDo.kt with project tasks and ideas
    - Create `ToDo.kt` to track pending tasks including bucket renaming, database field updates, and error handling improvements
    - Add ideas for future features such as an article screen for external links

---
+++ app/src/main/java/com/toure/mehedi/style_generator_ai/ToDo.kt
@@ -0,0 +1,12 @@
+package com.toure.mehedi.style_generator_ai
+
+
+// TODO : Change bucket name fashion_product to default_image
+// TODO : thumbnailUrl in supabase to thumbnailPath
+// TODO : Change bucket policies for fashion_product
+// TODO : negative_prompt in supabase to prompt
+// TODO : Need to handle all error from domain part using try-catch and Exception with sealed Class to log or show ui
+// TODO : Need to handle ui error - When bytes: ByteArray is null in FashionProductDetailViewModel
+
+// IDEA : Article screen from website or link or the app can
+// take a link from a market and get the article
