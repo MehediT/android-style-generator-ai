6b4cb93a7f3630deb7a0da11fe476e0060d6d83e
mehediT <mehedi.toure@gmail.com>
2026-02-24 02:16:18 +0100

    refactor: remove unused Supabase storage imports in TableSupabaseService
    - Remove unused `BucketApi` and `storage` imports from `TableSupabaseService`

--- data/src/main/kotlin/com/toure/mehedi/style_generator_ai/data/remote/service/TableSupabaseService.kt
+++ data/src/main/kotlin/com/toure/mehedi/style_generator_ai/data/remote/service/TableSupabaseService.kt
@@ -3,8 +3,6 @@ package com.toure.mehedi.style_generator_ai.data.remote.service
 import io.github.jan.supabase.SupabaseClient
 import io.github.jan.supabase.postgrest.from
 import io.github.jan.supabase.postgrest.query.PostgrestQueryBuilder
-import io.github.jan.supabase.storage.BucketApi
-import io.github.jan.supabase.storage.storage
 import javax.inject.Inject
 
 class TableSupabaseService @Inject constructor(
